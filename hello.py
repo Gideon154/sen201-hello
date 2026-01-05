71d6a1cc3165c9042972ebe284ad0663a5d5b925
Gideon154 <oguerigideon13@gmail.com>
2026-01-05 21:36:51 +0100

    Fixed Hello World Python program
    Updated header comments and removed redundant lines.

--- hello.py
+++ hello.py
@@ -1,10 +1,6 @@
-# SEN 201-ASSIGNMENT
-## Hello World Program
+# SEN 201 ASSIGNMENT
+# Hello World Program
+# Course: SEN 201
+# Language: Python
 
-**Course:** SEN 201
-
-**Language:** Python
-
-  ### Code:
-'''python
-  print("Hello, World")
+print("Hello, World")
